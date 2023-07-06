b85b810556f2001df4140ff612fc1a0f96f07722
b1gmouse <80082626+b1gmouse@users.noreply.github.com>
2023-07-07 06:16:15 +1000

    Update test.js

--- jsProj/test.js
+++ jsProj/test.js
@@ -1,23 +1,72 @@
-// класс с методом вкл/выкл
-class checkConnection{
-  constructor(name,isTurned)  {
-    this.isTurned = isTurned
-    this.name = name
+// класс с методом вкл/выкл E4.5
+class ElectricalAppliance
+  {
+    constructor(name, power, isTurned){
+      this.name = name;
+      this.power = power;
+      this.isPlugged = isTurned;
+    }
+    
+    checkPlugIn(){
+      if (this.isPlugged == 'on') console.log(this.name + " is plugged!")
+      else console.log(this.name + " is not plugged!")
+    }
+    
+    getPower(){
+      return this.power
+    }
   }
-  showPos() {
-    console.log('Your  ' + this.name + ' is turned ' + this.isTurned)
-  }
+
+const fridge = new ElectricalAppliance('fridge', 900, 'on');
+const pc = new ElectricalAppliance('pc', 850, 'on');
+
+console.log(fridge.getPower() + pc.getPower());
+fridge.checkPlugIn();
+pc.checkPlugIn();
+
+
+// E4.3
+function createObj(){
+  return Object.create(null)
 }
 
-class checkEfficiency extends checkConnection{
-  showEfficiency() {
-    console.log('Your '+ this.name + ' is turned '+ this.isTurned +' and work normally!')
-  }
+console.log(createObj())
+
+
+const myObject = {
+  age : 23
+}
+function printRes(object){
+  console.log('Hello world!')
+}
+printRes(myObject)
+
+
+function compareFeatures(object, feature){
+  if (feature in object) return true
+  else return false
+}
+console.log(compareFeatures(myObject, 'age'))
+
+// E4.4
+function ElectricalAppliance(name, power, isTurned) {
+  this.name = name;
+  this.power = power;
+  this.isPlugged = isTurned;
 }
 
-const lamp = new checkConnection('lamp', 'on');
-lamp.showPos();
-const tv = new checkConnection('tv', 'off');
-tv.showPos();
-const pc = new checkEfficiency('pc', 'on');
-pc.showEfficiency();
+ElectricalAppliance.prototype.checkPlugIn = function() {
+  if (this.isPlugged == 'on') console.log(this.name + " is plugged!")
+  else console.log(this.name + " is not plugged!");
+}
+
+ElectricalAppliance.prototype.getPower = function() {
+  return this.power;
+}
+
+const fridge = new ElectricalAppliance('fridge', 900, 'on');
+const pc = new ElectricalAppliance('pc', 850, 'on');
+
+console.log(fridge.getPower() + pc.getPower());
+fridge.checkPlugIn();
+pc.checkPlugIn();
